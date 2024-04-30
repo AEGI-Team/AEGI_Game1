41a6840deb6bf948d0c84b9e6b1c30ca10a8ba09
Элина Хаматвалиева <khamatvalievawork@gmail.com>
2024-04-30 20:28:50 +0500

    New

--- AEGI_Game/Form1.Designer.cs
+++ AEGI_Game/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AEGI_Game
 {
-    partial class Form1
+    partial class FormOurGame
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormOurGame));
+            timer = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBoxCar = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCar).BeginInit();
+            SuspendLayout();
+            // 
+            // timer
+            // 
+            timer.Enabled = true;
+            timer.Interval = 20;
+            timer.Tick += timer_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(840, 650);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(0, -650);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(840, 650);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBoxCar
+            // 
+            pictureBoxCar.Image = (Image)resources.GetObject("pictureBoxCar.Image");
+            pictureBoxCar.Location = new Point(548, 428);
+            pictureBoxCar.Name = "pictureBoxCar";
+            pictureBoxCar.Size = new Size(65, 128);
+            pictureBoxCar.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxCar.TabIndex = 1;
+            pictureBoxCar.TabStop = false;
+            // 
+            // FormOurGame
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            ClientSize = new Size(840, 650);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBoxCar);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormOurGame";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "OurGame";
+            Load += FormOurGame_Load;
+            KeyPress += FormOurGame_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCar).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+        private PictureBox pictureBoxWithCar;
+        private System.Windows.Forms.Timer timer;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBoxCar;
     }
 }
--- AEGI_Game/Form1.cs
+++ AEGI_Game/Form1.cs
@@ -1,10 +1,51 @@
 namespace AEGI_Game
 {
-    public partial class Form1 : Form
+    public partial class FormOurGame : Form
     {
-        public Form1()
+        public FormOurGame()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.White;
+        }
+
+        private void FormOurGame_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int speed = 10;
+            pictureBox1.Top += speed;
+            pictureBox3.Top += speed;
+
+            if (pictureBox1.Top >= 650)
+            {
+                pictureBox1.Top = 0;
+                pictureBox3.Top = -650;
+            }
+
+            pictureBoxCar.BringToFront();
+        }
+        /// <12314654
+
+        private void FormOurGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
